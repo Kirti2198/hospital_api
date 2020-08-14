a845a830f03106105c3c1fb7ea95bec07e1c8f6e
Kirti2198 <kirti2198@gmail.com>
2020-08-14 12:33:11 +0530

    define the models Schema

---
+++ models/patient.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const patientSchema = new Schema({
+    
+  phone: {
+      type: Number,
+      maxlength:10,
+      required: true,
+      unique:true,
+  },
+  name: {
+      type: String,
+      required:true,
+  },
+  doctor: {
+      type: Schema.Types.ObjectId,
+      ref:'Doctor'
+  }
+});
+
+
+const Patient = mongoose.model('Patient', patientSchema);
+module.exports = Patient;
